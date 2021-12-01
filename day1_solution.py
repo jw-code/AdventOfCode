90b8ae3786cb893a4e1a4dd65cc5bb5930aaf2ac
jw-code <28834873+jw-code@users.noreply.github.com>
2021-12-01 11:06:04 -0700

    Update day1_solution.py

--- day1_solution.py
+++ day1_solution.py
@@ -1,2 +1,5 @@
 depths = list(map(int, open('input.txt')))
+depths2 = list(sum(i) for i in zip(depths, depths[1:], depths[2:]))
+
 print(sum(b > a for a, b in zip(depths, depths[1:])))
+print(sum(b > a for a, b in zip(depths2, depths2[1:])))
